454ef9704760a5340779c7dff285247643ca192a
nateyoungren <nathanaelyoungren@gmail.com>
2019-03-13 13:05:31 -0700

    Create framework and connect to test project.

---
+++ Loading UI Test/Loading UI Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Loading UI Test
+//
+//  Created by Nathanael Youngren on 3/13/19.
+//  Copyright © 2019 Nathanael Youngren. All rights reserved.
+//
+
+import UIKit
+import Loading_UI
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
---
+++ Loading UI/Loading UI/Other Sources/Loading_UI.h
@@ -0,0 +1,19 @@
+//
+//  Loading_UI.h
+//  Loading UI
+//
+//  Created by Nathanael Youngren on 3/13/19.
+//  Copyright © 2019 Nathanael Youngren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Loading_UI.
+FOUNDATION_EXPORT double Loading_UIVersionNumber;
+
+//! Project version string for Loading_UI.
+FOUNDATION_EXPORT const unsigned char Loading_UIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Loading_UI/PublicHeader.h>
+
+
