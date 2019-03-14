d47ba2f727f9a08e35008f3f5d904b7a113ba8a7
nateyoungren <nathanaelyoungren@gmail.com>
2019-03-13 17:59:21 -0700

    Complete animation

--- Loading UI/Loading UI/LoadingViewController.swift
+++ Loading UI/Loading UI/LoadingViewController.swift
@@ -6,38 +6,53 @@
 //  Copyright © 2019 Nathanael Youngren. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 public class LoadingViewController: UIViewController {
     
     public override func viewDidLoad() {
-        animatingCircle()
+        showCircle()
         view.backgroundColor = backgroundColor ?? UIColor.white
     }
     
-    private func animatingCircle() {
-        layer = CAShapeLayer()
-
+    private func showCircle() {
         let circleSize = self.size ?? 30
-        
+
         let x = view.center.x - (circleSize / 2)
         let y = view.center.y - (circleSize / 2)
-        
+
         let circleFrame = CGRect(x: x, y: y, width: circleSize, height: circleSize)
         let circle = UIBezierPath(ovalIn: circleFrame)
         layer.path = circle.cgPath
         layer.fillColor = UIColor.clear.cgColor
-        
-        layer.strokeColor = self.color ?? UIColor.gray.cgColor
-        layer.lineWidth = self.thickness ?? 15
-        
-        // add layer to view
+        layer.lineCap = CAShapeLayerLineCap.round
+        layer.strokeEnd = 0
+
+        layer.strokeColor = self.color ?? UIColor.lightGray.cgColor
+        layer.lineWidth = self.width ?? 15
+
         view.layer.addSublayer(layer)
+
+        animate()
     }
-    
+
+    func animate() {
+        circleAnimation.keyPath = "strokeEnd"
+        circleAnimation.toValue = 1
+        circleAnimation.duration = 1
+        circleAnimation.fillMode = CAMediaTimingFillMode.forwards
+        circleAnimation.isRemovedOnCompletion = false
+        circleAnimation.repeatCount = .infinity
+        circleAnimation.timingFunction = CAMediaTimingFunction(name: .easeInEaseOut)
+
+        layer.add(circleAnimation, forKey: "load")
+    }
+
     public var backgroundColor: UIColor?
-    public var thickness: CGFloat?
+    public var width: CGFloat?
     public var color: CGColor?
     public var size: CGFloat?
-    private var layer: CAShapeLayer!
+    
+    private var layer = CAShapeLayer()
+    private var circleAnimation = CABasicAnimation()
 }
